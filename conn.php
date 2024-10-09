558957ae9d460424daf22bb44935edda923392c1
Gabriel Calderón <gabrielvilleda8@gmail.com>
2024-10-08 19:18:58 -0600

    feat: :fire: add php files

---
+++ conn.php
@@ -0,0 +1,27 @@
+<?php 
+
+    class DB extends PDO {
+        public function __construct($dsn, $username = NULL, $password = NULL, $options = []){
+
+            $default_options = [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ];
+
+            $options = array_replace($default_options);
+
+        }
+
+
+        public function run($sql, $args = null){
+            if($args) {
+                return $this->query($sql);
+            }
+            $stmt = $this->prepare($sql);
+        }
+
+
+    }
+
+?>
